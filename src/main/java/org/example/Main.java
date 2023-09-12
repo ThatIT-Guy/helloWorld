4e40f076fc734d3b4499eed7e35c866e9b081a07
ThatIT-Guy <patilshailesh484@gmail.com>
2023-09-13 01:33:25 +0530

    hello world code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+//when you use ln it informs java to break line and print the code on next line
+        System.out.println("Hello world!");
+        System.out.print("Hello world!");
         System.out.println("Hello world!");
     }
 }
