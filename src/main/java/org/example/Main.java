e122502fecf909c6d358bf4d9c2128311c97ce13
ThatIT-Guy <patilshailesh484@gmail.com>
2023-09-13 01:40:52 +0530

    hello world code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,11 @@ public class Main {
 //when you use ln it informs java to break line and print the code on next line
         System.out.println("Hello world!");
         System.out.print("Hello world!");
+//        another way to print line next is using \n
+        System.out.print("Hello world!\n");
+//        it also works in same as well
+        System.out.print("Hello world!\non the second line");
         System.out.println("Hello world!");
+
     }
 }
